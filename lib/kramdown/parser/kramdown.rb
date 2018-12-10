fef86182716fe38eff69a2c1aa915d620f2476ec
Raekye <Raekye@users.noreply.github.com>
2018-12-09 20:10:09 -0500

    disable tables

--- lib/kramdown/parser/kramdown.rb
+++ lib/kramdown/parser/kramdown.rb
@@ -74,7 +74,8 @@ module Kramdown
 
         @block_parsers = [:blank_line, :codeblock, :codeblock_fenced, :blockquote, :atx_header,
                           :horizontal_rule, :list, :definition_list, :block_html, :setext_header,
-                          :block_math, :table, :footnote_definition, :link_definition, :abbrev_definition,
+                          #:block_math, :table, :footnote_definition, :link_definition, :abbrev_definition,
+                          :block_math, :footnote_definition, :link_definition, :abbrev_definition,
                           :block_extensions, :eob_marker, :paragraph]
         @span_parsers =  [:emphasis, :codespan, :autolink, :span_html, :footnote_marker, :link, :smart_quotes, :inline_math,
                          :span_extensions, :html_entity, :typographic_syms, :line_break, :escaped_chars]
